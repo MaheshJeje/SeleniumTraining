43f52809d054c501a5746de93aa6c2f75348c532
MaheshJeje <jejurkar.mahesh@gmail.com>
2024-08-04 23:07:47 +0530

    Data Provder.

---
+++ src/test/java/datadriventesting/DataProvider1.java
@@ -0,0 +1,17 @@
+package datadriventesting;
+
+import org.testng.annotations.*;
+
+public class DataProvider1 {
+	
+	@Test
+	public void testCaseData() {
+		
+	}
+	
+	@DataProvider(name="driveTest")
+	public void getData() {
+		
+	}
+
+}
