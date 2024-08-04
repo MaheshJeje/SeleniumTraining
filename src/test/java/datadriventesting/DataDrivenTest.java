16754d0ac61b28fbfb41750cb6c4ef491d246404
MaheshJeje <jejurkar.mahesh@gmail.com>
2024-08-04 12:32:25 +0530

    Added files.

--- src/test/java/datadriventesting/DataDrivenTest.java
+++ src/test/java/datadriventesting/DataDrivenTest.java
@@ -1,22 +1,56 @@
 package datadriventesting;
 
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class DataDrivenTest {
-
-	public static void main(String[] args) throws Exception {
-		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"\\input-files\\TestData.xlsx");
+	
+	public ArrayList<String> getData(String testcaseName) throws Exception {
+		ArrayList<String> a = new ArrayList<String>();
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "\\input-files\\TestData.xlsx");
 		XSSFWorkbook workbook = new XSSFWorkbook(fis);
-		int noOfSheet =	workbook.getNumberOfSheets();
+		int noOfSheet = workbook.getNumberOfSheets();
 		System.out.println(noOfSheet);
-		
-		for(int i=0; i<noOfSheet; i++) {
-			if(workbook.getSheetName(i).equalsIgnoreCase("testdatasheet1")) {
+
+		for (int i = 0; i < noOfSheet; i++) {
+			if (workbook.getSheetName(i).equalsIgnoreCase("testdatasheet1")) {
 				XSSFSheet sheet = workbook.getSheetAt(i);
+				Iterator<Row> rows = sheet.iterator();
+				Row firstrow = rows.next();
+				Iterator<Cell> cell = firstrow.cellIterator();
+				int k = 0;
+				int column = 0;
+				while (cell.hasNext()) {
+					Cell value = cell.next();
+					if (value.getStringCellValue().equalsIgnoreCase("TestCases")) {
+						column = k;
+					}
+					k++;
+				}
+				System.out.println(column);
+
+				while (rows.hasNext()) {
+					Row r = rows.next();
+					if (r.getCell(column).getStringCellValue().equalsIgnoreCase(testcaseName)) {
+						Iterator<Cell> cv = r.cellIterator();
+						while (cv.hasNext()) {
+							a.add(cv.next().getStringCellValue());
+						}
+					}
+				}
 			}
 		}
+		
+		return a;
+	}
+
+	public static void main(String[] args) throws Exception {
+
 	}
 }
