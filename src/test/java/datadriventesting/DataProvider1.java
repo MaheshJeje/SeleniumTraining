823f18ded33fc25bd27caba7d73c0bcbe00337a2
MaheshJeje <jejurkar.mahesh@gmail.com>
2024-08-05 11:31:22 +0530

    Excel data provider code completed.

--- src/test/java/datadriventesting/DataProvider1.java
+++ src/test/java/datadriventesting/DataProvider1.java
@@ -2,39 +2,38 @@ package datadriventesting;
 
 import java.io.FileInputStream;
 
+import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.xssf.usermodel.*;
 import org.testng.annotations.*;
 
 public class DataProvider1 {
 
 	@Test(dataProvider = "driveTest")
-	public void testCaseData(String data1, String data2, int data3) {
+	public void testCaseData(String data1, String data2, String data3) {
 		System.out.println(data1 + " " + data2 + " " + data3);
 	}
 
-	//@DataProvider(name = "driveTest")
-	@Test
-	public void getData() throws Exception {
-//		Object[][] data = {{"Hello","Hi",1},{"Bye","Bye",2},{"Hello","Bye",3}};
-//		return data;
-
+	@DataProvider(name = "driveTest")
+	public Object[][] getData() throws Exception {
+		DataFormatter formatter = new DataFormatter();
 		FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "\\input-files\\TestData2.xlsx");
 		XSSFWorkbook workbook = new XSSFWorkbook(fis);
 		XSSFSheet sheet = workbook.getSheetAt(0);
-		XSSFRow row = sheet.getRow(0);
+		XSSFRow row = sheet.getRow(1);
 		int rowCount = sheet.getPhysicalNumberOfRows();
 		int columnCount = row.getLastCellNum();
 
-		Object[][] excelData = new Object[rowCount - 1][columnCount];
+		Object[][] excelData = new Object[rowCount-1][columnCount];
 
-		for (int i = 1; i < rowCount; i++) {
-			row = sheet.getRow(i);
-			columnCount = row.getLastCellNum();
+		for (int i = 0; i < rowCount-1; i++) {
+			row = sheet.getRow(i+1);
 			for (int j = 0; j < columnCount; j++) {
-			  System.out.print(row.getCell(j)+"\t");
+				XSSFCell cell = row.getCell(j);
+				excelData[i][j] = (formatter.formatCellValue(cell));
 			}
-			System.out.println();
 		}
+		return excelData;
+
 	}
 
 }
